3017760766a16ffd312136bd6d6b6976ef804e32
RAHEEL HASSAN <raheelhassanwork@gmail.com>
2026-08-27 17:05:32 +0500

    CustomeUser debugging

--- accounts/models.py
+++ accounts/models.py
@@ -5,11 +5,10 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
 
     class Role(models.TextChoices):
-        USER = "user", "User"
+        CANDIDATE = "candidate", "Candidate"
         COMPANY = "company", "Company"
 
     role = models.CharField(
         max_length=20,
         choices=Role.choices,
-        default=Role.USER
     )
