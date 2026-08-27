fca34c5879bb41e0f0861bd545c64b5b6ab2bf4b
RAHEEL HASSAN <raheelhassanwork@gmail.com>
2026-08-27 16:59:34 +0500

    CustomeUser

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractUser):
+
+    class Role(models.TextChoices):
+        USER = "user", "User"
+        COMPANY = "company", "Company"
+
+    role = models.CharField(
+        max_length=20,
+        choices=Role.choices,
+        default=Role.USER
+    )
