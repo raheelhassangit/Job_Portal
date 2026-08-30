7dee56605d5fdbef72e9d0ffbc29c819b5085dfb
RAHEEL HASSAN <raheelhassanwork@gmail.com>
2026-08-30 13:38:54 +0500

    profiles views

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from accounts.models import User
+from .forms import CandidateProfileForm, CompanyProfileForm
 
-# Create your views here.
+
+@login_required
+def profile_setup(request):
+    if request.user.role == User.Role.CANDIDATE:
+        profile = request.user.candidate_profile
+        form_class = CandidateProfileForm
+    else:
+        profile = request.user.company_profile
+        form_class = CompanyProfileForm
+
+    if request.method == "POST":
+        form = form_class(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect("profiles:profile_setup")
+    else:
+        form = form_class(instance=profile)
+
+    return render(request, "profiles/profile_setup.html", {"form": form})
