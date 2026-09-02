8a0b9d4a6dab1add9f93437f12a45f8d3596b6fe
RAHEEL HASSAN <raheelhassanwork@gmail.com>
2026-09-02 15:01:30 +0500

    Jobs urls

---
+++ jobs/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = "jobs"
+
+urlpatterns = [
+    path("", views.job_list, name="job_list"),
+    path("create/", views.job_create, name="job_create"),
+    path("<int:job_id>/", views.job_detail, name="job_detail"),
+    path("<int:job_id>/edit/", views.job_update, name="job_update"),
+    path("<int:job_id>/delete/", views.job_delete, name="job_delete"),
+]
