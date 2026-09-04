18bda137adb20c21134646300e60d52c17b6a5d7
RAHEEL HASSAN <raheelhassanwork@gmail.com>
2026-09-05 01:40:26 +0500

    Congig settings

--- config/settings.py
+++ config/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="127.0.0.1,localhost").split(",")
 INTERNAL_IPS = ["127.0.0.1"]
 
 AUTH_USER_MODEL = "accounts.User"
